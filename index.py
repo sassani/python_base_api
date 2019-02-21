400a5a81cce91cf2f6c43b47c2150a9f91118dd3
Ardavan Sassani <a.sassani@gmail.com>
2019-02-21 11:57:03 -0500

    add appSettings.dev and clean unused libraries

--- index.py
+++ index.py
@@ -3,10 +3,15 @@ import json
 from flask import Flask
 
 
-APP_SETTINGS = json.load(open('appSettings.json'))
-
 APP = Flask(__name__)
+app_setting_file = 'appSettings.json'
+if APP.config['ENV'] == 'development':
+    app_setting_file = 'appSettings.dev.json'
+
+APP_SETTINGS = json.load(open(app_setting_file))
+
 APP.config['SQLALCHEMY_DATABASE_URI'] = APP_SETTINGS['database']['mySqlDsn']
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 # pylint: disable=unused-import, wrong-import-position
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,3 +1,1 @@
-from dotenv import load_dotenv
 from index import APP as application
-load_dotenv()
